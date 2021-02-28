7146f8aaaf6b95631203bd4c3f0e8cf6c5665383
nagievakbar <nagievakbar@gmail.com>
2021-02-28 21:21:49 +0500

    new update#5

--- makereport/utils.py
+++ makereport/utils.py
@@ -15,10 +15,11 @@ def get_verifyPkcs7(report_id):
         body = """<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
             <Body>
                 <verifyPkcs7 xmlns="http://v1.pkcs7.plugin.server.dsv.eimzo.yt.uz/">
-                    <pkcs7B64 xmlns=""> {} < / pkcs7B64 > < / verifyPkcs7 >
-        < / Body >
-    
-        < / Envelope > """.format(each)
+                    <pkcs7B64 xmlns=""> {} 
+                    </pkcs7B64 >
+                </verifyPkcs7 >
+            </Body>
+        </Envelope> """.format("each")
 
         response = requests.post(url, data=body, headers=headers)
 
