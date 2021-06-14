149a96cc252028c5425b2d3546d3b13b4cfa4a0e
Bob <bob@example.com>
2021-06-15 00:23:57 +0500

    debug false

--- makereport/utils.py
+++ makereport/utils.py
@@ -418,7 +418,10 @@ class CustomPaginator(Paginator):
             next = self.num_pages
             return range(previous, next)
         else:
+
             remainder = self._page_custom.number % 10
+            if self._page_custom.number == 1:
+                remainder += 1
             to_ten = 10 - remainder
             previous = self._page_custom.number - remainder
             next = self._page_custom.number + to_ten + 1
